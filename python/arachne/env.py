8dbb3242672fb2747a2987b6b91252cf53e85fa9
Takafumi Kubota <takafumi.kubota@fixstars.com>
2021-07-20 18:22:28 +0900

    [poetry/pip] updates for publishing an arachne pip package

--- python/arachne/env.py
+++ python/arachne/env.py
@@ -5,18 +5,12 @@ from pathlib import Path
 
 
 class Env(object):
-    PYTHON_DIR = Path(path.abspath(path.join(__file__, "..", "..")))
-    ROOT_DIR = PYTHON_DIR.parent
-    THIRDPARTY_DIR = ROOT_DIR / "3rdparty"
+    ROOT_DIR = Path(path.abspath(path.join(__file__, "..")))
     ARTIFACT_DIR = ROOT_DIR / ".artifacts"
     DATASET_DIR = Path("/datasets")
     MODEL_DIR = ROOT_DIR / ".models"
     BUILD_CACHE_DIR = ROOT_DIR / ".cache"
 
-
-assert Env.PYTHON_DIR.exists()
-assert Env.ROOT_DIR.exists()
-assert Env.THIRDPARTY_DIR.exists()
 if Env.ARTIFACT_DIR.exists():
     if Env.ARTIFACT_DIR.is_dir():
         shutil.rmtree(Env.ARTIFACT_DIR)
