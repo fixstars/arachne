1f4cc7b44e5af52b64294c02cebbd73e55a10f98
Takafumi Kubota <takafumi.kubota@fixstars.com>
2021-08-05 15:00:21 +0900

    [pipeline/stage/compiler/tvm] reformat

--- python/arachne/pipeline/stage/compiler/tvm.py
+++ python/arachne/pipeline/stage/compiler/tvm.py
@@ -1,6 +1,6 @@
-from abc import ABCMeta, abstractclassmethod, abstractmethod, abstractstaticmethod
+from abc import ABCMeta, abstractmethod
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional
 
 import tvm.autotvm
 import tvm.driver.tvmc as tvmc
@@ -10,10 +10,10 @@ import tvm.driver.tvmc.frontends as tvmcfrontends
 from arachne.pipeline.package import (
     DarknetPackage,
     DarknetPackageInfo,
-    ONNXPackage,
-    ONNXPackageInfo,
     KerasPackage,
     KerasPackageInfo,
+    ONNXPackage,
+    ONNXPackageInfo,
     Package,
     PackageInfo,
     TF1Package,
@@ -35,23 +35,25 @@ from ..stage import Parameter, Stage
 
 
 class TVMCompilerBase(Stage, metaclass=ABCMeta):
+    """A base class for TVMCompiler & TVMVMCompiler"""
+
     @staticmethod
     @abstractmethod
     def get_name() -> str:
         raise NotImplementedError()
-    
+
     @staticmethod
     @abstractmethod
     def _OutputPackage(**kwargs):
         raise NotImplementedError()
-    
+
     @staticmethod
     @abstractmethod
     def _OutputPackageInfo(**kwargs):
         raise NotImplementedError()
 
     @classmethod
-    def get_output_info(cls, input: PackageInfo, params: Parameter) -> Optional[PackageInfo]:        
+    def get_output_info(cls, input: PackageInfo, params: Parameter) -> Optional[PackageInfo]:
         params = TVMCompilerBase.extract_parameters(params)
         target = params["target"]
         target_host = params["target_host"]
@@ -74,7 +76,9 @@ class TVMCompilerBase(Stage, metaclass=ABCMeta):
             return None
         if isinstance(input, TFLitePackageInfo) and input.for_edgetpu:
             return None
-        return cls._OutputPackageInfo(target=target, target_host=target_host, target_tvmdev=target_tvmdev)
+        return cls._OutputPackageInfo(
+            target=target, target_host=target_host, target_tvmdev=target_tvmdev
+        )
 
     @staticmethod
     def extract_parameters(params: Parameter) -> Parameter:
@@ -84,12 +88,13 @@ class TVMCompilerBase(Stage, metaclass=ABCMeta):
         return {"target": target, "target_host": target_host}
 
     @staticmethod
-    def compile_model():
+    @abstractmethod
+    def compile_model(model, target: str, target_host: str, output_dir: Path, filename: str):
         raise NotImplementedError()
 
     @classmethod
     def process(cls, input: Package, params: Parameter, output_dir: Path) -> Package:
-        params = TVMCompiler.extract_parameters(params)
+        params = cls.extract_parameters(params)
         target = params["target"]
         assert target is not None
         target_host = params["target_host"]
@@ -99,11 +104,21 @@ class TVMCompilerBase(Stage, metaclass=ABCMeta):
         filename = "tvm_package.tar"
 
         assert isinstance(
-            input, (TFLitePackage, TorchScriptPackage, DarknetPackage, TF1Package, KerasPackage, ONNXPackage)
+            input,
+            (
+                TFLitePackage,
+                TorchScriptPackage,
+                DarknetPackage,
+                TF1Package,
+                KerasPackage,
+                ONNXPackage,
+            ),
         )
         if isinstance(input, DarknetPackage):
             input_filename = input.weight_file
-        elif isinstance(input, (TFLitePackage, TorchScriptPackage, TF1Package, KerasPackage, ONNXPackage)):
+        elif isinstance(
+            input, (TFLitePackage, TorchScriptPackage, TF1Package, KerasPackage, ONNXPackage)
+        ):
             input_filename = input.model_file
 
         if isinstance(input, TF1Package):
@@ -114,18 +129,14 @@ class TVMCompilerBase(Stage, metaclass=ABCMeta):
                 outputs=input.output_info.keys(),
             )
         elif isinstance(input, ONNXPackage):
-            model = tvmcfrontends.load_model(str(input.dir / input_filename), shape_dict=shape_dict, freeze_params=True)
+            model = tvmcfrontends.load_model(
+                str(input.dir / input_filename), shape_dict=shape_dict, freeze_params=True
+            )
         else:
             model = tvmcfrontends.load_model(str(input.dir / input_filename), shape_dict=shape_dict)
-        
-        cls.compile_model(
-            model,
-            target,
-            target_host,
-            output_dir,
-            filename            
-        )
-        
+
+        cls.compile_model(model, target, target_host, output_dir, filename)
+
         return cls._OutputPackage(
             dir=output_dir,
             input_info=input.input_info,
@@ -135,9 +146,10 @@ class TVMCompilerBase(Stage, metaclass=ABCMeta):
             target_tvmdev=target_tvmdev,
             package_file=Path(filename),
         )
-        
+
 
 class TVMCompiler(TVMCompilerBase):
+    """A stage for compiling a dnn model via tvm.relay.build()"""
     @staticmethod
     def get_name() -> str:
         return "tvm_compiler"
@@ -145,7 +157,7 @@ class TVMCompiler(TVMCompilerBase):
     @staticmethod
     def _OutputPackage(**kwargs) -> TVMPackage:
         return TVMPackage(**kwargs)
-    
+
     @staticmethod
     def _OutputPackageInfo(**kwargs) -> TVMPackageInfo:
         return TVMPackageInfo(**kwargs)
@@ -155,7 +167,7 @@ class TVMCompiler(TVMCompilerBase):
         tvm_target, _ = tvmccommon.target_from_cli(target)
         if tvm_target.kind.name == "cuda" and "arch" in tvm_target.attrs:
             tvm.autotvm.measure.measure_methods.set_cuda_target_arch(tvm_target.attrs["arch"])
-            
+
         output_path = output_dir / filename
         tvmc.compiler.compile_model(
             model,
@@ -166,6 +178,7 @@ class TVMCompiler(TVMCompilerBase):
             target_host=target_host,
             desired_layout=None,
         )
+
 
 register_stage(TVMCompiler)
 
--- python/arachne/pipeline/stage/compiler/tvm_vm.py
+++ python/arachne/pipeline/stage/compiler/tvm_vm.py
@@ -1,25 +1,22 @@
 from pathlib import Path
-from tensorflow.python.ops.gen_array_ops import pad_eager_fallback
 
-from arachne.pipeline.package import (
-    TVMVMPackage,
-    TVMVMPackageInfo,
-)
+from arachne.pipeline.package import TVMVMPackage, TVMVMPackageInfo
 
 from .._registry import register_stage, register_stage_candidate
-from ..stage import Parameter, Stage
 from . import TVMCompilerBase
 
 
 class TVMVMCompiler(TVMCompilerBase):
+    """A stage for compiling a dnn model via tvm.relay.vm.build()"""
+
     @staticmethod
     def get_name() -> str:
         return "tvm_vm_compiler"
-    
+
     @staticmethod
     def _OutputPackage(**kwargs) -> TVMVMPackage:
         return TVMVMPackage(**kwargs)
-    
+
     @staticmethod
     def _OutputPackageInfo(**kwargs) -> TVMVMPackageInfo:
         return TVMVMPackageInfo(**kwargs)
@@ -31,27 +28,31 @@ class TVMVMCompiler(TVMCompilerBase):
                 import tvm.driver.tvmc.common as common
                 import tvm.driver.tvmc.composite_target as composite_target
                 from tvm.target import Target
+
                 tvm_target, extra_targets = common.target_from_cli(target)
                 tvm_target, target_host = Target.check_and_update_host_consist(
-                    tvm_target, target_host)
+                    tvm_target, target_host
+                )
 
                 for codegen_from_cli in extra_targets:
-                    codegen = composite_target.get_codegen_by_target(
-                        codegen_from_cli["name"])
+                    codegen = composite_target.get_codegen_by_target(codegen_from_cli["name"])
                     partition_function = codegen["pass_pipeline"]
                     mod, codegen_config = partition_function(
-                        mod, params, **codegen_from_cli["opts"])
+                        mod, params, **codegen_from_cli["opts"]
+                    )
                 return mod, params, tvm_target
 
             def build(mod, params, tvm_target, package_path):
-                from tvm.relay import vm as relay_vm
                 import tarfile
                 import tempfile
+
+                from tvm.relay import vm as relay_vm
+
                 vm_exec = relay_vm.compile(mod, target=tvm_target)
                 lib = vm_exec.mod
                 with tempfile.TemporaryDirectory() as tmpdirname:
-                    lib_name = 'mod.tar'
-                    path_lib = tmpdirname + '/' + lib_name
+                    lib_name = "mod.tar"
+                    path_lib = tmpdirname + "/" + lib_name
                     lib.export_library(path_lib)
                     with tarfile.open(package_path, "w") as tar:
                         tar.add(path_lib, lib_name)
@@ -59,9 +60,10 @@ class TVMVMCompiler(TVMCompilerBase):
             mod, params = model.mod, model.params
             mod, params, tvm_target = annotation(mod, params, target, target_host)
             build(mod, params, tvm_target, package_path)
-        
+
         output_path = output_dir / filename
         compile_vm(model, target, target_host, output_path)
+
 
 register_stage(TVMVMCompiler)
 
