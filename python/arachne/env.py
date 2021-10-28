94b0a5722330076b04d772da79514887ed65b047
Takafumi Kubota <takafumi.kubota@fixstars.com>
2021-10-28 17:33:58 +0900

    update env.py to follow the XDG base directory specification

--- python/arachne/env.py
+++ python/arachne/env.py
@@ -1,14 +1,16 @@
 import os
 import shutil
-from os import path
 from pathlib import Path
+
+if not os.environ.get("HOME"):
+    raise RuntimeError(
+        "You have to setup ${HOME} directory because the arachne library follow the XDG Base Directory specification"
+    )
 
 
 class Env(object):
-    ROOT_DIR = Path(path.abspath(path.join(__file__, "..")))
-    ARTIFACT_DIR = ROOT_DIR / ".artifacts"
-    MODEL_DIR = ROOT_DIR / ".models"
-    BUILD_CACHE_DIR = ROOT_DIR / ".cache"
+    CACHE_HOME = Path(os.environ.get("HOME")) / ".cache" / "arachne"
+    ARTIFACT_DIR = CACHE_HOME / "artifacts"
 
 
 if Env.ARTIFACT_DIR.exists():
@@ -16,6 +18,4 @@ if Env.ARTIFACT_DIR.exists():
         shutil.rmtree(Env.ARTIFACT_DIR)
     else:
         os.remove(Env.ARTIFACT_DIR)
-Env.ARTIFACT_DIR.mkdir()
-Env.MODEL_DIR.mkdir(exist_ok=True)
-Env.BUILD_CACHE_DIR.mkdir(exist_ok=True)
+Env.ARTIFACT_DIR.mkdir(parents=True)
