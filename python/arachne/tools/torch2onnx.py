df46c16b244c4f58e9bc45ef04050e6e2228f7a0
Takafumi Kubota <takafumi.kubota@fixstars.com>
2021-12-21 16:19:41 +0900

    implement tool: torch2onnx

---
+++ python/arachne/tools/torch2onnx.py
@@ -0,0 +1,104 @@
+import itertools
+from dataclasses import dataclass, field
+from typing import Any, List, Optional
+
+import hydra
+import torch
+import torch.onnx
+import torch.onnx.symbolic_helper
+from hydra.core.config_store import ConfigStore
+from hydra.utils import to_absolute_path
+from omegaconf import MISSING, DictConfig, OmegaConf
+
+from arachne.utils import get_model_spec, get_torch_dtype_from_string, save_model
+
+from ..data import Model
+
+
+@dataclass
+class Torch2ONNXConfig:
+    export_params: bool = True
+    verbose: bool = False
+    training: int = torch.onnx.TrainingMode.EVAL.value
+    operator_export_type: Optional[int] = None
+    opset_version: int = torch.onnx.symbolic_helper._default_onnx_opset_version
+    do_constant_folding: bool = False
+    dynamic_axes: Optional[Any] = None
+    keep_initializers_as_inputs: Optional[bool] = None
+    custom_opsets: Optional[Any] = None
+
+
+def register_torch2onnx_config() -> None:
+    cs = ConfigStore.instance()
+    group_name = "tools"
+    cs.store(
+        group=group_name,
+        name="torch2onnx",
+        package="tools.torch2onnx",
+        node=Torch2ONNXConfig,
+    )
+
+
+def run(input: Model, cfg: Torch2ONNXConfig) -> Model:
+    idx = itertools.count().__next__()
+    filename = f"model_{idx}.onnx"
+
+    assert input.spec is not None
+
+    model = torch.load(input.file)
+
+    args = []
+    for inp in list(input.spec.inputs):
+        x = torch.randn(*inp.shape, dtype=get_torch_dtype_from_string(inp.dtype))
+        args.append(x)
+    args = tuple(args)
+
+    torch.onnx.export(
+        model=model,
+        args=args,
+        f=filename,
+        export_params=cfg.export_params,
+        verbose=cfg.verbose,
+        training=cfg.training,
+        operator_export_type=cfg.operator_export_type,
+        opset_version=cfg.opset_version,
+        do_constant_folding=cfg.do_constant_folding,
+        dynamic_axes=cfg.dynamic_axes,
+        custom_opsets=cfg.custom_opsets,
+    )
+    return Model(filename, spec=get_model_spec(filename))
+
+
+@hydra.main(config_path=None, config_name="config")
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+    input_model_path = to_absolute_path(cfg.input)
+    output_path = to_absolute_path(cfg.output)
+
+    input_model = Model(file=input_model_path, spec=get_model_spec(input_model_path))
+
+    # overwrite model spec if input_spec is specified
+    if cfg.input_spec:
+        input_model.spec = OmegaConf.load(to_absolute_path(cfg.input_spec))  # type: ignore
+
+    assert input_model.spec is not None
+    output_model = run(input=input_model, cfg=cfg.tools.torch2onnx)
+    save_model(model=output_model, output_path=output_path, cfg=cfg)
+
+
+if __name__ == "__main__":
+    register_torch2onnx_config()
+
+    from ..config.base import BaseConfig
+
+    defaults = [{"tools": "torch2onnx"}, "_self_"]
+
+    @dataclass
+    class Config(BaseConfig):
+        defaults: List[Any] = field(default_factory=lambda: defaults)
+        tools: Any = MISSING
+
+    cs = ConfigStore.instance()
+    cs.store(name="config", node=Config)
+    main()
--- python/arachne/utils.py
+++ python/arachne/utils.py
@@ -1,87 +1,129 @@
+import dataclasses
 import platform
 import subprocess
 import tarfile
 import tempfile
+from dataclasses import asdict
 from typing import Optional
 
 import onnx
 import onnxruntime
 import tensorflow as tf
+import torch
 import yaml
 from omegaconf import DictConfig, OmegaConf
 
 from .data import Model, ModelSpec, TensorSpec
 
 
-def get_model_spec(model_file: str) -> Optional[ModelSpec]:
+def get_torch_dtype_from_string(dtype_str: str) -> torch.dtype:
+    dtype_str_to_torch_dtype_dict = {
+        "bool": torch.bool,
+        "uint8": torch.uint8,
+        "int8": torch.int8,
+        "int16": torch.int16,
+        "int32": torch.int32,
+        "int64": torch.int64,
+        "float16": torch.float16,
+        "float32": torch.float32,
+        "float64": torch.float64,
+        "complex64": torch.complex64,
+        "complex128": torch.complex128,
+    }
+    if dtype_str not in dtype_str_to_torch_dtype_dict:
+        assert False, f"Not conversion map for {dtype_str}"
+    return dtype_str_to_torch_dtype_dict[dtype_str]
+
+
+def get_tflite_model_spec(model_file: str) -> ModelSpec:
     inputs = []
     outputs = []
+    interpreter = tf.lite.Interpreter(model_path=model_file)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    for inp in input_details:
+        inputs.append(
+            TensorSpec(name=inp["name"], shape=inp["shape"].tolist(), dtype=inp["dtype"].__name__)
+        )
+    for out in output_details:
+        outputs.append(
+            TensorSpec(name=out["name"], shape=out["shape"].tolist(), dtype=out["dtype"].__name__)
+        )
+    return ModelSpec(inputs=inputs, outputs=outputs)
+
+
+def get_keras_model_spec(model_file: str) -> ModelSpec:
+    inputs = []
+    outputs = []
+    model = tf.keras.models.load_model(model_file)
+    for inp in model.inputs:  # type: ignore
+        shape = [-1 if x is None else x for x in inp.shape]
+        inputs.append(TensorSpec(name=inp.name, shape=shape, dtype=inp.dtype.name))
+    for out in model.outputs:  # type: ignore
+        shape = [-1 if x is None else x for x in out.shape]
+        outputs.append(TensorSpec(name=out.name, shape=shape, dtype=out.dtype.name))
+    return ModelSpec(inputs=inputs, outputs=outputs)
+
+
+def get_saved_model_spec(model_file: str) -> ModelSpec:
+    inputs = []
+    outputs = []
+    model = tf.saved_model.load(model_file)
+    model_inputs = [
+        inp for inp in model.signatures["serving_default"].inputs if "unknown" not in inp.name  # type: ignore
+    ]
+    model_outputs = [
+        out for out in model.signatures["serving_default"].outputs if "unknown" not in out.name  # type: ignore
+    ]
+    for inp in model_inputs:
+        shape = [-1 if x is None else x for x in inp.shape]
+        inputs.append(TensorSpec(name=inp.name, shape=shape, dtype=inp.dtype.name))
+    for out in model_outputs:
+        shape = [-1 if x is None else x for x in out.shape]
+        outputs.append(TensorSpec(name=out.name, shape=shape, dtype=out.dtype.name))
+    return ModelSpec(inputs=inputs, outputs=outputs)
+
+
+def get_onnx_model_spec(model_file: str) -> ModelSpec:
+    inputs = []
+    outputs = []
+
+    session = onnxruntime.InferenceSession(model_file)
+    for inp in session.get_inputs():
+        dtype = inp.type.replace("tensor(", "").replace(")", "")
+        if dtype == "float":
+            dtype = "float32"
+        elif dtype == "double":
+            dtype = "float64"
+        inputs.append(TensorSpec(name=inp.name, shape=inp.shape, dtype=dtype))
+    for out in session.get_outputs():
+        dtype = out.type.replace("tensor(", "").replace(")", "")
+        if dtype == "float":
+            dtype = "float32"
+        elif dtype == "double":
+            dtype = "float64"
+        outputs.append(TensorSpec(name=out.name, shape=out.shape, dtype=dtype))
+    return ModelSpec(inputs=inputs, outputs=outputs)
+
+
+def get_model_spec(model_file: str) -> Optional[ModelSpec]:
     if model_file.endswith(".tflite"):
-
-        interpreter = tf.lite.Interpreter(model_path=model_file)
-        interpreter.allocate_tensors()
-
-        # Get input and output tensors.
-        input_details = interpreter.get_input_details()
-        output_details = interpreter.get_output_details()
-
-        for inp in input_details:
-            inputs.append(
-                TensorSpec(
-                    name=inp["name"], shape=inp["shape"].tolist(), dtype=inp["dtype"].__name__
-                )
-            )
-        for out in output_details:
-            outputs.append(
-                TensorSpec(
-                    name=out["name"], shape=out["shape"].tolist(), dtype=out["dtype"].__name__
-                )
-            )
+        return get_tflite_model_spec(model_file)
     elif model_file.endswith(".h5"):
-        model = tf.keras.models.load_model(model_file)
-        for inp in model.inputs:  # type: ignore
-            shape = [-1 if x is None else x for x in inp.shape]
-            inputs.append(TensorSpec(name=inp.name, shape=shape, dtype=inp.dtype.name))
-        for out in model.outputs:  # type: ignore
-            shape = [-1 if x is None else x for x in out.shape]
-            outputs.append(TensorSpec(name=out.name, shape=shape, dtype=out.dtype.name))
+        return get_keras_model_spec(model_file)
     elif model_file.endswith("saved_model"):
-        model = tf.saved_model.load(model_file)
-        model_inputs = [
-            inp for inp in model.signatures["serving_default"].inputs if "unknown" not in inp.name  # type: ignore
-        ]
-        model_outputs = [
-            out for out in model.signatures["serving_default"].outputs if "unknown" not in out.name  # type: ignore
-        ]
-        for inp in model_inputs:
-            shape = [-1 if x is None else x for x in inp.shape]
-            inputs.append(TensorSpec(name=inp.name, shape=shape, dtype=inp.dtype.name))
-        for out in model_outputs:
-            shape = [-1 if x is None else x for x in out.shape]
-            outputs.append(TensorSpec(name=out.name, shape=shape, dtype=out.dtype.name))
+        return get_saved_model_spec(model_file)
+    elif model_file.endswith(".onnx"):
+        return get_onnx_model_spec(model_file)
     elif model_file.endswith(".pb"):
         return None
-    elif model_file.endswith(".onnx"):
-
-        session = onnxruntime.InferenceSession(model_file)
-        for inp in session.get_inputs():
-            dtype = inp.type.replace("tensor(", "").replace(")", "")
-            if dtype == "float":
-                dtype = "float32"
-            elif dtype == "double":
-                dtype = "float64"
-            inputs.append(TensorSpec(name=inp.name, shape=inp.shape, dtype=dtype))
-        for out in session.get_outputs():
-            dtype = out.type.replace("tensor(", "").replace(")", "")
-            if dtype == "float":
-                dtype = "float32"
-            elif dtype == "double":
-                dtype = "float64"
-            outputs.append(TensorSpec(name=out.name, shape=out.shape, dtype=dtype))
-    elif model_file.endswith(".pth"):
+    elif model_file.endswith(".pth") or model_file.endswith(".pt"):
         return None
-
-    return ModelSpec(inputs=inputs, outputs=outputs)
+    return None
 
 
 def get_tensorrt_version():
@@ -110,7 +152,13 @@ def get_cudnn_version():
 
 
 def save_model(model: Model, output_path: str, cfg: DictConfig):
-    env = {"model_spec": OmegaConf.to_object(model.spec), "dependencies": []}
+    if isinstance(model.spec, DictConfig):
+        spec = OmegaConf.to_object(model.spec)
+    elif dataclasses.is_dataclass(model.spec):
+        spec = asdict(model.spec)
+    else:
+        assert False, f"model.spec is unknown object or None: {model.spec}"
+    env = {"model_spec": spec, "dependencies": []}
     if "tvm" in cfg.tools.keys():
         env["tvm_device"] = "cpu"
         targets = list(cfg.tools.tvm.composite_target)
