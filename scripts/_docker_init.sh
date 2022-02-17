1e3f1560ede13e5c95edf737a1e6349f1e2f9f5c
Takafumi Kubota <takafumi.kubota@fixstars.com>
2022-02-17 18:01:22 +0900

    [docker] add scripts for running a dev container from CLI

---
+++ scripts/_docker_init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# overwrite uid and gid
+usermod -u $HOST_UID developer
+groupmod -g $HOST_GID developer
+
+# keep some environments
+echo "export PYTHONPATH=${PYTHONPATH}" >> /home/developer/.bashrc
+echo "export TVM_LIBRARY_PATH=${TVM_LIBRARY_PATH}" >> /home/developer/.bashrc
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> /home/developer/.bashrc
+echo "export PATH=${PATH}" >> /home/developer/.bashrc
+echo "cd /workspaces/arachne" >> /home/developer/.bashrc
+
+# change to the developer
+chown developer:developer -R /home/developer
+su - developer
