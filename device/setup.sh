fe6d482796b59b79bd2f5b16578e8778234db778
Yasuhiro Nitta <yasuhiro.nitta@fixstars.com>
2022-03-01 08:32:09 +0000

    add environment directory name to input arguments

--- device/setup.sh
+++ device/setup.sh
@@ -3,19 +3,19 @@
 set -euo pipefail
 
 if [ $# -ne 2 ]; then
-    echo "Usage: . setup.sh <runtime_name> <port>"
+    echo "Usage: . setup.sh <env_dirname> <runtime_name> <port>"
     exit 1
 fi
-runtime_name=$1
-rpc_port=$2
+env_dirname = $1
+runtime_name=$2
+rpc_port=$3
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 tvm_dir=${script_dir}/../3rdparty/tvm
 build_dir=${script_dir}/../build/tvm
 arachne_root=${script_dir}/..
 
-source ${script_dir}/env/.venv/bin/activate
-pip list | grep tensorflow
+source ${script_dir}/${env_dirname}/.venv/bin/activate
 export LD_LIBRARY_PATH=${build_dir}:$LD_LIBRARY_PATH
 export PYTHONPATH=${tvm_dir}/python:${arachne_root}/python:$PYTHONPATH
-echo $PYTHONPATH
+export PATH=/usr/local/cuda/bin:${PATH}
 python -m arachne.runtime.rpc.server --port ${rpc_port} --runtime ${runtime_name}
